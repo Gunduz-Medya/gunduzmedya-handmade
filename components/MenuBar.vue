db549675c5134f135d70eb9ecc735b065786bb86
Barış Gündüz <baris.gunduz@devstup.org>
2021-12-28 20:53:56 +0300

    update

---
+++ components/MenuBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar app color="white" flat>
+      <v-container class="py-0 fill-height">
+        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive>
+           <NuxtLink :to="menu.home">Anasayfa</NuxtLink>
+           <NuxtLink :to="menu.about">Hakkımızda</NuxtLink>
+           <NuxtLink :to="menu.projects">Projelerimiz</NuxtLink>
+           <NuxtLink :to="menu.expertise">Hizmetlerimiz</NuxtLink>
+           <NuxtLink :to="menu.contact">İletişim</NuxtLink>
+        </v-responsive>
+
+      </v-container>
+    </v-app-bar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menu: {
+        home: '/',
+        about: '/hakkimizda',
+        projects: '/projelerimiz',
+        expertise: '/hizmetlerimiz',
+        contact: '/iletisim'
+      },
+    }
+  },
+}
+</script>
+<style lang="sass">
+.v-application a
+  color: #5c5c5c
+  text-decoration: none
+  padding: 0px 5px 0px 5px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,84 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-app id="inspire">
+    <MenuBar />
+    <v-main class="white">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <v-carousel v-model="model">
+                <v-carousel-item v-for="(color, i) in colors" :key="color">
+                  <v-sheet :color="color" height="100%" tile>
+                    <v-row class="fill-height" align="center" justify="center">
+                      <div class="text-h2">Slide {{ i + 1 }}</div>
+                    </v-row>
+                  </v-sheet>
+                </v-carousel-item>
+              </v-carousel>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <!-- <h1>Gündüz Medya</h1>
+              <v-divider style="padding-bottom: 40px"></v-divider> -->
+              <v-row>
+                <v-col cols="4">
+                  <h3>Dijital medya <span>Video İçerikleri</span></h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque nulla quibusdam impedit a pariatur, cum aperiam
+                    molestias sequi! Quae voluptas rerum magnam officiis non
+                    eaque quidem aut! Adipisci, repellendus. Asperiores!
+                  </p>
+                </v-col>
+                <v-col cols="4">
+                  <h3>Dijital medya</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque nulla quibusdam impedit a pariatur, cum aperiam
+                    molestias sequi! Quae voluptas rerum magnam officiis non
+                    eaque quidem aut! Adipisci, repellendus. Asperiores!
+                  </p>
+                </v-col>
+                <v-col cols="4">
+                  <h3>Dijital medya</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque nulla quibusdam impedit a pariatur, cum aperiam
+                    molestias sequi! Quae voluptas rerum magnam officiis non
+                    eaque quidem aut! Adipisci, repellendus. Asperiores!
+                  </p>
+                </v-col>
+              </v-row>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+      <Footer />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    links: ['Hakkımızda', 'Projelerimiz', 'Hizmetlerimiz', 'İletişim'],
+    model: 0,
+    colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+  }),
+}
+</script>
+<style>
+h3 {
+    font-size: 2.3333333333em;
+    letter-spacing: -1px;
+    margin-bottom: 1.75rem;
+    color: #454545;
+}
+span {
+    display: block;
+    color: #fe2d5b;
+}
+</style>
