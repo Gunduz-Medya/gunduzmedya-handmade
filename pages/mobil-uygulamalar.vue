7550eed7aa719e2ecdf4955ce7ab9c9184fbe469
Barış Gündüz <baris.gunduz@devstup.org>
2022-04-07 22:17:57 +0300

    mobile apps page

--- pages/mobil-uygulamalar.vue
+++ pages/mobil-uygulamalar.vue
@@ -27,10 +27,80 @@
     <v-row>
       <v-col>
         <v-sheet min-height="70vh" rounded="lg">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          accusantium eos, aut voluptates eligendi totam quisquam deleniti
-          voluptatibus ipsa! Blanditiis ab neque officiis quibusdam iusto
-          accusantium minima alias magnam itaque.
+          <v-row>
+            <v-col cols="12">
+              <h4>Web Projeler</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <div class="col-lg-6">
+              <h3>Evrensel <span>Uygulama Geliştirme</span></h3>
+              <p>
+                Mobil geliştiricilerimiz hem yerel platformlarda hem de
+                JavaScript'te deneyim sahibidir. React-Native sayesinde iOS ve
+                Andorid için bakımı kolay ve anlaşılması kolay kod tabanları
+                oluşturuyoruz.
+              </p>
+            </div>
+            <div class="col-lg-6">
+              <h3>Takip <span> Pazarlama</span></h3>
+              <p>
+                Kullanıcı eylemlerini nasıl izleyeceğinizi biliyoruz ve bu
+                eylemlerden kararlar almanıza izin veriyoruz. Firebase, Mixpanel
+                vb. ile olan deneyimimiz, her kullanıcıyı takip edebilecek ve
+                onlarla kolayca etkileşime geçebileceksiniz.
+              </p>
+            </div>
+            <div class="col-lg-6">
+              <h3>Gerçek Zamanlı Veri <span> Mesajlaşma</span></h3>
+              <p>
+                Gerçek zamanlı veri akışları ve mesajlaşma konusundaki
+                uzmanlığımız sayesinde, kullanıcılarınız herhangi bir bildirimi
+                zamanında alacak ve uygulamanızın sohbete ihtiyacı varsa,
+                diğerleriyle gerçek zamanlı olarak sorunsuz bir şekilde
+                etkileşime geçecektir.
+              </p>
+            </div>
+            <div class="col-lg-6">
+              <h3>Otomatize Edilmiş<span> Geliştirme</span></h3>
+              <p>
+                "Bir şey otomatikleştirilebiliyorsa, onu otomatik hale getir"
+                demeyi seviyoruz. Geliştirici ekibimiz, insan hatasını mümkün
+                olduğunca az yapmak için ilk taahhütten beta sürümüne kadar her
+                şeyi otomatik hale getirir.
+              </p>
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet min-height="70vh" rounded="lg">
+          <v-row>
+            <v-col cols="12">
+              <h4>Araçlar ve Teknolojiler</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <div class="col-lg-3 gm_dflex">
+              <img
+                class="img-13"
+                src="~/assets/img/tech-stack/firebase.svg"
+                alt=""
+              />
+              <span class="tech-stack-text">Firebase</span>
+            </div>
+            <div class="col-lg-3 gm_dflex">
+              <img
+                class="img-13"
+                src="~/assets/img/tech-stack/react-native.svg"
+                alt=""
+              />
+              <span class="tech-stack-text">React Native</span>
+            </div>
+
+          </v-row>
         </v-sheet>
       </v-col>
     </v-row>
@@ -42,32 +112,7 @@
               <h4>Gündüz Medya</h4>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col md="12" sm="12" lg="6">
-              <h3 class="gm-color-red">Biz Kimiz?</h3>
-              <p class="pb40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                nulla quibusdam impedit a pariatur, cum aperiam molestias sequi!
-                Quae voluptas rerum magnam officiis non eaque quidem aut!
-                Adipisci, repellendus. Asperiores!
-              </p>
-              <nuxt-link class="gm-bordered-button" to="/hakkimizda"
-                ><span>Hakkımızda</span></nuxt-link
-              >
-            </v-col>
-            <v-col md="12" sm="12" lg="6">
-              <h3 class="gm-color-red">Neler Yapıyoruz?</h3>
-              <p class="pb40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                nulla quibusdam impedit a pariatur, cum aperiam molestias sequi!
-                Quae voluptas rerum magnam officiis non eaque quidem aut!
-                Adipisci, repellendus. Asperiores!
-              </p>
-              <nuxt-link class="gm-bordered-button" to="/hizmetlerimiz"
-                ><span>Hizmetlerimiz</span></nuxt-link
-              >
-            </v-col>
-          </v-row>
+          <ContentFooter />
         </v-sheet>
       </v-col>
     </v-row>
